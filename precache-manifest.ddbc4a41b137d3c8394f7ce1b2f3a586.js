9251cdd415a4ddb4e2cf5bc31397f92f88d4536a
Alexei Dias <alexeidias@gmail.com>
2019-10-28 14:02:23 -0700

    Updates

---
+++ precache-manifest.ddbc4a41b137d3c8394f7ce1b2f3a586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfef2ac2bd41f6099c86d24acc71406",
+    "url": "/ClickGame/index.html"
+  },
+  {
+    "revision": "ac5699374dd3e5df5361",
+    "url": "/ClickGame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "76df4d833b87e1ea3574",
+    "url": "/ClickGame/static/js/2.d83b082e.chunk.js"
+  },
+  {
+    "revision": "ac5699374dd3e5df5361",
+    "url": "/ClickGame/static/js/main.ed65826d.chunk.js"
+  },
+  {
+    "revision": "ab11ff29259a89697804",
+    "url": "/ClickGame/static/js/runtime-main.9ae02213.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ClickGame/static/media/logo.25bf045c.svg"
+  }
+]);
